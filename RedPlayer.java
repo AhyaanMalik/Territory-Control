80e946eaec0d115ad62e2c94c236703aaa98934d
AhyaanMalik <ak27mali@siena.edu>
2026-03-29 19:34:25 -0400

    Starting to add the first player

---
+++ RedPlayer.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+public class RedPlayer extends ColorPlayer {
+
+    public RedPlayer() {
+        super(Color.RED, 0);
+    }
+
+    @Override
+    public void next(Simulator simulator) {
+        
+    }
+    
+}
